a45410da02410953fc057486370eba8173bd84ad
Noor-al <no.ab.2727@gmail.com>
2025-09-15 15:25:22 -0400

    fixed function to return emotions only with the dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url =  'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +7,11 @@ def emotion_detector(text_to_analyse):
     input_json = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json = input_json, headers=headers)
+    response = json.loads(response.text)
 
-    print(response.text)
+    emotions = response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
